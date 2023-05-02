f604639ee103cbf647c8e70ca47c5ebb25a87af9
homeworkdome <homeworkdome@outlook.com>
2023-05-02 20:24:31 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingRibbon/Main.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingRibbon
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string tabName = "Revit API training";
+            application.CreateRibbonTab(tabName);
+            string utilsFolderPath = @"C:\APPLICATIONS\RevitAPITraining\";
+
+            var panel = application.CreateRibbonPanel(tabName, "Элементы");
+
+            var button = new PushButtonData("Элементы", "Идентификатор элементов", Path.Combine(utilsFolderPath, "RAPITUI.dll"), "RAPITUI.Main");
+
+            panel.AddItem(button);
+
+            return Result.Succeeded;
+        }
+    }
+}
